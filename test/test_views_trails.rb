ddcc4f883b796d8f79afae2271f72ff25482118f
Jay McGavren <jay@mcgavren.com>
2007-07-14 00:13:24 +0000

    animate() now sleeps 1/60 second between frames for smoother animation. populate() for adding specified number of creatures added. Creature set now varies per test. test_accelerate added. test_turn added. test_approach added (tests vector addition).

--- test/test_views_trails.rb
+++ test/test_views_trails.rb
@@ -25,19 +25,6 @@ class TestTrailsView < Test::Unit::TestCase
 		
 		#Create environment with objects.
 		@environment = Environment.new
-		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(1, 0, 0), Vector.new(10, 45))
-		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(0, 1, 0), Vector.new(20, 135))
-		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(0, 0, 1), Vector.new(30, 225))
-		i = 0
-		while i < 1 do
-			@environment.objects << Creature.new(
-				i,
-				Location.new(@view.width * i, @view.height * i),
-				Color.new(i, 1 - i, i / 2 + 0.5),
-				Vector.new(100 * i, i * 360)
-			)
-			i += 0.01
-		end
 		
 	end
 	
@@ -52,6 +39,7 @@ class TestTrailsView < Test::Unit::TestCase
 			(1..frame_count).each do |i|
 				@view.render(@environment.objects)
 				@environment.interact
+				sleep 1.0 / 60.0
 			end
 		rescue Exception => exception
 			puts exception
@@ -59,8 +47,26 @@ class TestTrailsView < Test::Unit::TestCase
 	end
 	
 	
+	def populate(environment, count)
+		count.times do |i|
+			multiplier = i / count.to_f
+			environment.objects << Creature.new(
+				i,
+				Location.new(multiplier * @view.width, multiplier * @view.height),
+				Color.new(multiplier, 1 - multiplier, multiplier / 2 + 0.5),
+				Vector.new(100 * multiplier, multiplier * 360)
+			)
+		end
+	end
+	
+	
 	def test_render
 	
+		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(1, 0, 0), Vector.new(10, 45))
+		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(0, 1, 0), Vector.new(20, 135))
+		@environment.objects << Creature.new(nil, Location.new(@view.width/2, @view.height/2), Color.new(0, 0, 1), Vector.new(30, 225))
+		populate(@environment, 100)
+		
 		thread = Thread.new do
 		
 			#Test rendering.
@@ -82,6 +88,8 @@ class TestTrailsView < Test::Unit::TestCase
 	
 	def test_environmental_factors
 	
+		populate(@environment, 100)
+		
 		thread = Thread.new do
 		
 			#Test normally.
@@ -104,6 +112,8 @@ class TestTrailsView < Test::Unit::TestCase
 	
 	def test_behaviors
 	
+		populate(@environment, 50)
+		
 		#Add target and have all creatures chase it.
 		@environment.objects.each do |creature|
 			chase = Behavior.new
@@ -125,7 +135,7 @@ class TestTrailsView < Test::Unit::TestCase
 		)
 			
 		thread = Thread.new {animate(FRAME_COUNT)}
-				
+		
 	end
 	
 	
@@ -150,6 +160,8 @@ class TestTrailsView < Test::Unit::TestCase
 	
 	def test_change_color
 
+		populate(@environment, 100)
+		
 		@environment.objects << Morpher.new(nil, Location.new(0, 100), Color.new(1, 0, 0), Vector.new(100, 0))
 		@environment.objects << Morpher.new(nil, Location.new(0, 150), Color.new(0, 1, 0), Vector.new(200, 0))
 		@environment.objects << Morpher.new(nil, Location.new(0, 200), Color.new(0, 0, 1), Vector.new(300, 0))
@@ -159,19 +171,114 @@ class TestTrailsView < Test::Unit::TestCase
 	end
 	
 	
+	def test_accelerate
+	
+		populate(@environment, 100)
+		
+		#Keep a separate clock for each object.
+		clocks = Hash.new {|h, k| h[k] = Clock.new}
+		accelerate = Behavior.new
+		accelerate.actions << lambda do |creature, target|
+			#Accelerate the appropriate amount for the elapsed time.
+			creature.vector.speed += 100 * clocks[creature].elapsed_time
+		end
+		
+		#Add behavior to creatures.
+		@environment.objects.each {|creature| creature.behaviors << accelerate}
+		
+		thread = Thread.new {animate(FRAME_COUNT)}
+		
+	end
+	
+	
+	def test_turn
+	
+		populate(@environment, 100)
+		
+		#Keep a separate clock for each object.
+		clocks = Hash.new {|h, k| h[k] = Clock.new}
+		#Create a behavior.
+		turn = Behavior.new
+		turn.actions << lambda do |creature, target|
+			#Turn the appropriate amount for the elapsed time.
+			creature.vector.pitch += 30 * clocks[creature].elapsed_time
+		end
+		
+		#Add behavior to creatures.
+		@environment.objects.each {|creature| creature.behaviors << turn}
+		
+		thread = Thread.new {animate(FRAME_COUNT)}
+		
+	end
+	
+	
+	def test_approach
+	
+		populate(@environment, 20)
+		
+		#Keep a separate heading for each object.
+		headings = Hash.new {|h, k| h[k] = Vector.new(k.vector.speed, k.vector.pitch)}
+		
+		#Create a behavior.
+		max_turn_angle = 20
+		approach = Behavior.new
+		approach.actions << lambda do |creature, target|
+		
+			#Find the difference between the current heading and the angle to the target.
+			turn_angle = Utility.find_angle(creature.location, target.location) - headings[creature].pitch
+			
+			#If the angle is the long way around from the current heading, change it to the smaller angle.
+			if turn_angle > 180 then
+				turn_angle -= 360.0
+			elsif turn_angle < -180 then
+				turn_angle += 360.0
+			end
+			
+			#If turn angle is greater than allowed turn speed, reduce it.
+			turn_angle = Utility.constrain_value(turn_angle, max_turn_angle)
+			
+			#Turn the appropriate amount for the elapsed time.
+			headings[creature].pitch += turn_angle
+			
+			#Apply the heading to the creature's movement vector.
+			creature.vector += headings[creature]
+			
+		end
+		
+		#Target only the creature's prey.
+		approach.conditions << lambda do |creature, target|
+			target.tags.include?('prey')
+		end
+		
+		#Add behavior to creatures.
+		@environment.objects.each {|creature| creature.behaviors << approach}
+		
+		#Add a target.
+		@environment.objects << Creature.new(
+			"target",
+			Location.new(@view.width / 2, @view.height / 2),
+			Color.new(1, 1, 1),
+			Vector.new(3, 0),
+			0, #Age.
+			["prey"] #Tags.
+		)
+		
+		thread = Thread.new {animate(FRAME_COUNT * 50)}
+		
+	end
+	
+	
 end
 
 
-# # c:\work\zyps\source\net\sourceforge\zyps\actions\accelerateaction.java
 # # c:\work\zyps\source\net\sourceforge\zyps\actions\approachaction.java
-# c:\work\zyps\source\net\sourceforge\zyps\actions\destroyaction.java
 # # c:\work\zyps\source\net\sourceforge\zyps\actions\fleeaction.java
+# c:\work\zyps\source\net\sourceforge\zyps\actions\destroyaction.java
 # c:\work\zyps\source\net\sourceforge\zyps\actions\followinputdeviceaction.java
 # c:\work\zyps\source\net\sourceforge\zyps\actions\mateaction.java
 # c:\work\zyps\source\net\sourceforge\zyps\actions\reviveaction.java
 # # c:\work\zyps\source\net\sourceforge\zyps\actions\spawnaction.java
 # # c:\work\zyps\source\net\sourceforge\zyps\actions\tagaction.java
-# # c:\work\zyps\source\net\sourceforge\zyps\actions\turnaction.java
 # c:\work\zyps\source\net\sourceforge\zyps\behavior.java
 # # c:\work\zyps\source\net\sourceforge\zyps\clock.java
 # # c:\work\zyps\source\net\sourceforge\zyps\color.java
