df398f1171acacaa04b5505772ddee487edcd984
Jay McGavren <jay@mcgavren.com>
2007-10-20 03:19:10 +0000

    Version updated.

--- rakefile.rb
+++ rakefile.rb
@@ -9,7 +9,7 @@ require 'rake/testtask'
 
 #Configuration variables.
 PRODUCT_NAME = "Zyps"
-PRODUCT_VERSION = "0.5.2"
+PRODUCT_VERSION = "0.6.1"
 SUMMARY = "A game library for Ruby"
 AUTHOR = "Jay McGavren"
 AUTHOR_EMAIL = "jay@mcgavren.com"
