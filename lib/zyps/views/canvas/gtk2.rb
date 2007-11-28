9c53525bf2753770dc992556bb6144abc424e633
Jay McGavren <jay@mcgavren.com>
2007-11-28 18:10:57 +0000

    Canvas::GTK2 created. TrailsView#canvas attribute changed to take a Canvas::X class.

---
+++ lib/zyps/views/canvas/gtk2.rb
@@ -0,0 +1,138 @@
+# Copyright 2007 Jay McGavren, jay@mcgavren.com.
+# 
+# This file is part of Zyps.
+# 
+# Zyps is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+require 'gtk2'
+
+
+module Zyps::Canvas
+
+
+class GTK2
+
+
+	#A Gtk::DrawingArea that will be painted on.
+	attr_reader :drawing_area
+	#Dimensions of the drawing area.
+	attr_reader :width, :height
+
+	#Takes a hash with these keys and defaults:
+	#	:width => 600
+	#	:height => 400
+	def initialize (options = {})
+	
+		options = {
+			:width => 600,
+			:height => 400,
+		}.merge(options)
+		@width = options[:width]
+		@height = options[:height]
+
+		#Create a drawing area.
+		@drawing_area = Gtk::DrawingArea.new
+		#Set to correct size.
+		resize
+		
+		#Whenever the drawing area needs updating...
+		@drawing_area.signal_connect("expose_event") do
+			#Copy buffer bitmap to canvas.
+			@drawing_area.window.draw_drawable(
+				@drawing_area.style.fg_gc(@drawing_area.state), #Gdk::GC (graphics context) to use when drawing.
+				buffer, #Gdk::Drawable source to copy onto canvas.
+				0, 0, #Pull from upper left of source.
+				0, 0, #Copy to upper left of canvas.
+				-1, -1 #-1 width and height signals to copy entire source over.
+			)
+		end
+		
+	end
+
+	def width= (pixels) #:nodoc:
+		@width = pixels
+		resize
+	end
+	
+	def height= (pixels) #:nodoc:
+		@height = pixels
+		resize
+	end
+	
+	
+	def draw_rectangle(options = {})
+		options = {
+			:filled => true
+		}.merge(options)
+		graphics_context = Gdk::GC.new(buffer)
+		graphics_context.rgb_fg_color = convert_color(options[:color])
+		buffer.draw_rectangle(
+			graphics_context,
+			options[:filled],
+			options[:x], options[:y],
+			options[:width], options[:height]
+		)
+	end
+
+	
+	def draw_line(options = {})
+		graphics_context = Gdk::GC.new(buffer)
+		graphics_context.rgb_fg_color = convert_color(options[:color])
+		graphics_context.set_line_attributes(
+			options[:width].ceil,
+			Gdk::GC::LINE_SOLID,
+			options[:round_ends] ? Gdk::GC::CAP_ROUND : Gdk::GC::CAP_BUTT,
+			Gdk::GC::JOIN_MITER #Only used for polygons.
+		)
+		buffer.draw_line(
+			graphics_context,
+			options[:x1], options[:y1],
+			options[:x2], options[:y2]
+		)
+	end
+	
+	
+	def render
+		@drawing_area.queue_draw_area(0, 0, @width, @height)
+	end
+				
+	
+	private
+	
+		#Converts a Zyps Color to a Gdk::Color.
+		def convert_color(color)
+			Gdk::Color.new(
+				color.red * 65535,
+				color.green * 65535,
+				color.blue * 65535
+			)
+		end
+	
+	
+		def resize
+			@drawing_area.set_size_request(@width, @height)
+			@buffer = nil #Causes buffer to reset its size next time it's accessed.
+		end
+	
+		
+		def buffer
+			@buffer ||= Gdk::Pixmap.new(@drawing_area.window, @width, @height, -1)
+		end
+	
+	
+end
+
+
+end #module Zyps::Canvas
--- lib/zyps/views/trails.rb
+++ lib/zyps/views/trails.rb
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-require 'gtk2'
+require 'zyps'
 
 
 module Zyps
@@ -25,7 +25,7 @@ module Zyps
 #A view of game objects.
 class TrailsView
 
-	#A Gtk::DrawingArea to render objects on.
+	#A GUI toolkit-specific drawing area that will be used to render the view.
 	attr_reader :canvas
 	#Dimensions of the view.
 	attr_reader :width, :height
@@ -35,6 +35,7 @@ class TrailsView
 	attr_accessor :erase_flag
 
 	#Takes a hash with these keys and defaults:
+	#	:canvas => nil
 	#	:width => 600
 	#	:height => 400
 	#	:trail_length => 5
@@ -47,34 +48,20 @@ class TrailsView
 			:trail_length => 5,
 			:erase_flag => true
 		}.merge(options)
-		@width, @height, @trail_length, = options[:width], options[:height], options[:trail_length]
 		@width = options[:width]
 		@height = options[:height]
 		@trail_length = options[:trail_length]
 		@erase_flag = options[:erase_flag]
+		@canvas = options[:canvas]
+		
+		#Set canvas's size to match view's.
+		resize if @canvas
 	
-		#Create a drawing area.
-		@canvas = Gtk::DrawingArea.new
-		#Set to correct size.
-		resize
-		
-		#Whenever the drawing area needs updating...
-		@canvas.signal_connect("expose_event") do
-			#Copy buffer bitmap to canvas.
-			@canvas.window.draw_drawable(
-				@canvas.style.fg_gc(@canvas.state), #Gdk::GC (graphics context) to use when drawing.
-				buffer, #Gdk::Drawable source to copy onto canvas.
-				0, 0, #Pull from upper left of source.
-				0, 0, #Copy to upper left of canvas.
-				-1, -1 #-1 width and height signals to copy entire source over.
-			)
-		end
-		
 		#Track a list of locations for each object.
 		@locations = Hash.new {|h, k| h[k] = Array.new}
 		
 	end
-	
+
 	def width= (pixels) #:nodoc:
 		@width = pixels
 		resize
@@ -83,23 +70,25 @@ class TrailsView
 		@height = pixels
 		resize
 	end
-
+	def canvas= (canvas) #:nodoc:
+		@canvas = canvas
+		resize
+	end
+	
+	
 	#Takes an Environment, and draws it to the canvas.
 	#Tracks the position of each GameObject over time so it can draw a trail behind it.
 	#The head will match the object's Color exactly, fading to black at the tail.
 	#GameObject.size will be used as the line thickness at the object's head, diminishing to 1 at the tail.
 	def update(environment)
 	
-		graphics_context = Gdk::GC.new(buffer)
-		
 		#Clear the background on the buffer.
 		if @erase_flag
-			graphics_context.rgb_fg_color = Gdk::Color.new(0, 0, 0)
-			buffer.draw_rectangle(
-				graphics_context,
-				true, #Filled.
-				0, 0, #Lower-left corner.
-				@width, @height #Upper-right corner.
+			@canvas.draw_rectangle(
+				:color => Color.new(0, 0, 0),
+				:filled => true,
+				:x => 0, :y => 0,
+				:width => @width, :height => @height
 			)
 		end
 		
@@ -122,50 +111,38 @@ class TrailsView
 				#Divide the current segment number by trail segment count to get the multiplier to use for brightness and width.
 				multiplier = index.to_f / @locations[object.identifier].length.to_f
 				
-				#Set the drawing color to use the object's colors, adjusted by the multiplier.
-				graphics_context.rgb_fg_color = Gdk::Color.new( #Don't use Gdk::GC.foreground= here, as that requires a color to be in the color map already.
-					object.color.red * multiplier * 65535,
-					object.color.green * multiplier * 65535,
-					object.color.blue * multiplier * 65535
-				)
-				
-				#Multiply the actual drawing width by the current multiplier to get the current drawing width.
-				graphics_context.set_line_attributes(
-					(object_radius * 2 * multiplier).ceil,
-					Gdk::GC::LINE_SOLID,
-					Gdk::GC::CAP_ROUND, #Line ends drawn as semicircles.
-					Gdk::GC::JOIN_MITER #Only used for polygons.
-				)
-				
 				#Get previous location so we can draw a line from it.
 				previous_location = @locations[object.identifier][index - 1]
 				
-				#Draw a line with the current width from the prior location to the current location.
-				buffer.draw_line(
-					graphics_context,
-					previous_location[0], previous_location[1],
-					location[0], location[1]
+				@canvas.draw_line(
+					:color => Color.new(
+						object.color.red * multiplier,
+						object.color.green * multiplier,
+						object.color.blue * multiplier
+					),
+					:width => object_radius * 2 * multiplier,
+					:round_ends => true,
+					:x1 => previous_location[0], :y1 => previous_location[1],
+					:x2 => location[0], :y2 => location[1]
 				)
 				
 			end
 			
 		end
 		
-		@canvas.queue_draw_area(0, 0, @width, @height)
+		@canvas.render
 		
 	end
 	
 	
 	private
 	
+
 		def resize
-			@canvas.set_size_request(@width, @height)
-			@buffer = nil #Causes buffer to reset its size next time it's accessed.
+			@canvas.width = @width
+			@canvas.height = @height
 		end
-		
-		def buffer
-			@buffer ||= Gdk::Pixmap.new(@canvas.window, @width, @height, -1)
-		end
+
 	
 end
 
