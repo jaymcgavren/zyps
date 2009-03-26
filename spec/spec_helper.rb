ea610a398801457d8276fb9d5d01ccb4915b76b4
Jay McGavren <jay@mcgavren.com>
2009-03-26 14:05:50 -0700

    Added spec_helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'spec'
+require 'zyps'
+require 'zyps/actions'
+require 'zyps/conditions'
+require 'zyps/environmental_factors'
+require 'zyps/remote'
+require 'zyps/shapes'
+require 'zyps/views'
+require 'zyps/views/canvas'
+
+gems_loaded = false
+begin
+	require 'spec'
+rescue LoadError
+	if gems_loaded == false
+		require 'rubygems'
+		gems_loaded = true
+		retry
+	else
+		raise
+	end
+end
