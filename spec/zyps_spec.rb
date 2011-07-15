69da2686f100cf7d998ba69c5188b3b78ad6a0a6
Jay McGavren <jay@mcgavren.com>
2011-07-15 00:34:55 -0700

    spec_helper fixes.

--- spec/zyps_spec.rb
+++ spec/zyps_spec.rb
@@ -1,5 +1,10 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
+require 'zyps'
+require 'zyps/actions'
+require 'zyps/conditions'
+require 'zyps/environmental_factors'
+
 include Zyps
 
 
