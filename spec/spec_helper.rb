7ca5c9c8eacc472327df6011b89fa475224aefef
Jay McGavren <jay@mcgavren.com>
2020-03-28 16:56:30 -0700

    Modernize spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'rubygems'
-require 'rspec'
+require "bundler/setup"
+require "zyps"
+require "pry"
 
 #Allowed margin of error for be_close.
 MARGIN = 0.01
 
 RSpec.configure do |config|
-  
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
 end
