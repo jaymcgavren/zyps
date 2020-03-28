70f735a05fd8396a0e238c52cdb4fe1d77d075c4
Jay McGavren <jay@mcgavren.com>
2020-03-28 16:57:36 -0700

    Modernize rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,20 +1,6 @@
-require 'rubygems'
-require 'bundler'
-Bundler::GemHelper.install_tasks
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 
-require 'rspec/core/rake_task'
-RSpec::Core::RakeTask.new
-
-RSpec::Core::RakeTask.new(:rcov) do |t|
-  t.rcov = true
-  t.rcov_opts = "--exclude spec,gems\/"
-end
+RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
-
-require 'rake/rdoctask'
-Rake::RDocTask.new do |rdoc|
-  rdoc.title = "Zyps"
-  rdoc.rdoc_files.include('README*')
-  rdoc.rdoc_files.include('lib/**/*.rb')
-end
